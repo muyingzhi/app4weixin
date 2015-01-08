aacbe2f834f1a6e77df58ba0359d464c4f21c017
muyz.york <yingzhi.mu@live.cn>
2015-01-08 21:48:15 +0800

    增加hjk4wechat模块

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use('/users', users);
 var appWechat = require('./appWechat');
 app.use('/EHRBrowser',appWechat);
 
+//-----
+var appHJK = require('./hjk4wechat');
+app.use('/hjk4wechat',appHJK)
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');

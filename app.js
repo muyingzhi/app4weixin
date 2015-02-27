1e6a1928c0f2a9598f275ca15fb8b198c4a087e3
muyz.york <yingzhi.mu@live.cn>
2015-02-27 17:39:06 +0800

    端口号从环境变量设置PORT

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-process.env.PORT=8080;
-console.log("listen to "+process.env.PORT);
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +16,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
 app.use('/users', users);
-//app.use(express.favicon());
 //-----微信接口目录
 var appWechat = require('./appWechat');
 app.use('/EHRBrowser',appWechat);
@@ -37,7 +32,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -49,15 +43,14 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    // res.render('error', {
-    //     message: err.message,
-    //     error: {}
-    // });
-    res.send(err.message);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+    //res.send(err.message);
 });
 module.exports = app;
